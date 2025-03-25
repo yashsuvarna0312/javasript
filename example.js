daec60b04cd0d37237091a7301618a5db9099d19
yashsuvarna0312 <yashsuvarna0312@gmail.com>
2025-03-25 15:15:25 +0530

    Add files via upload

---
+++ example.js
@@ -0,0 +1,7 @@
+var number = 100
+function info(){
+    var name ="John"
+    console.log(name)
+    console.log(number)
+}
+info()
---
+++ string_method.js
@@ -0,0 +1,18 @@
+var str="Hello world!";
+console.log(str.charAt(0));
+var str ="Hello world!";
+console.log(str.charCodeAt(0));
+var str1="Hello";
+var str2="World!";
+console.log(str1.concat(" ",str2));
+var str="Hello World!";
+console.log(str.endsWith("World!"));
+var str="Hello World!";
+console.log(str.toUpperCase());
+var str="Hello World!";
+console.log(str.toLowerCase());
+var str="Hello World!";
+console.log(str.length);
+var str1="Hello World!";
+var str2="World!";
+console.log(str2.slice(0,3));
